86b2fd80862be73bb25cfe9403feefa39d1649f6
OhJin-Soo <162969776+OhJin-Soo@users.noreply.github.com>
2024-04-13 17:08:58 +0900

    Add files via upload

---
+++ hw5.py
@@ -0,0 +1,16 @@
+def read_single_digit(n) :
+    index = ['영','일','이','삼','사','오','육','칠','팔','구']
+    if 1 <= n <= 9 :
+        return index[n-1]
+
+def read_number(n) :
+    index = ['영','일','이','삼','사','오','육','칠','팔','구']
+    if 100 <= n <= 999 :
+        n_str = str(n)
+        result = ''
+        for digit in n_str :
+            result += index[int(digit)] +' '
+    return result.strip()
+
+n = int(input('세 자리 정수 입력 : '))
+print(read_number(n))
